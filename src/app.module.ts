c86c9ec653da6e085b7ab34726317f4346a081ad
caio <caiothalles607@gmail.com>
2024-03-05 14:55:36 -0300

    fix: add authentication on routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { MailModule } from './modules/mail/mail.module';
 import { AuthModule } from './modules/authenticate/authenticate.module';
+import { EnsureAuthenticateMidllwware } from './shared/http/middleware/ensure-authenticate-midlleware';
 
 @Module({
   imports: [UserModule, MailModule, AuthModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(EnsureAuthenticateMidllwware)
+      .exclude(
+        { path: '/user', method: RequestMethod.POST },
+        { path: '/user/all', method: RequestMethod.GET },
+        { path: '/user/:id', method: RequestMethod.DELETE },
+        { path: '/authentiacte', method: RequestMethod.POST },
+      )
+      .forRoutes('*');
+  }
+}
